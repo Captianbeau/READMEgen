95216940b043b5decf148b442a771d2c6dcd56d2
Capti <captianbeau@gmail.com>
2023-12-14 15:07:44 -0700

    added question structure

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
+const inquirer = require('inquirer')
+// TODO: Create an array of questions for user input  
+//email, username, title, description, Installation, usage, Contributing, tests, questions
+inquirer
+    .createPromptModule([
+        {
+            type:'input',
+            message:'What is your GitHub username?',
+            name:'username',
+        },
+        {
+            type:'input',
+            message:'What is your Email?',
+            name:'email',
+        },
+        {
+            type:'input',
+            message:'What is the title of your project?',
+            name:'title',
+        },
+        {
+            type:'input',
+            message:'Type a description of your Project.',
+            name:'description',
+        },
+        {
+            type:'input',
+            message:'Type installation instructions.',
+            name:'installation',
+        },
+        {
+            type:'input',
+            message:'Type how to use your project.',
+            name:'usage',
+        },
+        {
+            type:'input',
+            message:'How can others contribute?',
+            name:'contributing',
+        },
+        {
+            type:'input',
+            message:'Type how to test your project.',
+            name:'tests',
+        },
+    ]).then((answer)=>{
+        
+    })
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile('README', `# ${} \n ## Description \n`)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
